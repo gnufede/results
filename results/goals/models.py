9751f8f782e41984b05e41519508a568e7f85337
Federico Mon <gnu.fede@gmail.com>
2013-12-30 21:04:55 +0100

    First commit

---
+++ results/goals/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.db import models
+from model_utils.models import TimeStampedModel
+from datetime import date
+
+
+# Create your models here.
+class Category(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=500, blank=True)
+    parent = models.ForeignKey('Goal', related_name="%(app_label)s_%(class)s",
+              blank=True, null=True)
+    owner = models.ForeignKey(User)
+
+
+class Goal(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=500, blank=True)
+    date = models.DateField()
+    parent = models.ForeignKey('Goal', related_name="%(app_label)s_%(class)s",
+              blank=True, null=True)
+    tags = models.ManyToManyField(Category)
+    monthly = models.BooleanField()
+    owner = models.ForeignKey(User)
+
+    def getGoals(self, user, monthly=False):
+        today = date.today()
+        if monthly today = todaye
+        return goals
+        return Goal.objects.filter(owner=user, monthly=monthly, date=today)
+
+    def save(self):
+        if len(self.getGoals(self.owner, monthly=self.monthly)) > 2:
+            #TODO: raise errors here
+            pass
+        else:
+            super(Goal,self).save()
+
+
+class Win(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=500, blank=True)
+    date = models.DateField()
+    goal = models.ForeignKey('Goal', related_name='wins', blank=True,
+            null=True)
+    tags = models.ManyToManyField('Category')
+    monthly = models.BooleanField()
+    owner = models.ForeignKey(User)
+
+    def getWins(self, user, monthly=False):
+        today = date.today()
+        return Win.objects.filter(owner=user, monthly=monthly, date=today)
+
+    def save(self):
+        if len(self.getWins(self.owner, monthly=self.monthly)) > 2:
+            #TODO: raise errors here
+            pass
+        else:
+            super(Win,self).save()
+
